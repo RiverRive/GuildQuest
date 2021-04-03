5250db6081768691a40d51d827008aad02febe1a
Ryan Driscoll <ryan.driscoll@mines.sdsmt.edu>
2021-04-03 11:06:24 -0600

    added config php file and account php file

---
+++ account.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Accounts</title>
+</head>
+
+<body bgcolor = Azure>
+
+<h1>All Accounts</h1>
+
+
+
+</body>
+
+</html>
---
+++ guildQuestConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "127.0.0.1";			// localhost
+$user = "MySQL username";		// username for MySQL
+$password = "MySQL password";		// password for MySQL
+$dbname = "database Name";		// our database name
+$port = 3306				// default port for MySQL
+
+?>
