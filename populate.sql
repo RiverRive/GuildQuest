5a8a231eee38af9ea748d00b5edf535eb3e60224
Ryan Driscoll <ryan.driscoll@mines.sdsmt.edu>
2021-04-03 10:52:50 -0600

    added test data to account entity

---
+++ populate.sql
@@ -0,0 +1,19 @@
+--populate Account Entity
+
+-- PERHAPS CHANGE USERID TO AN INT?? --
+INSERT INTO ACCOUNT
+VALUES('001', 'rcdriscoll', 'targyiscool', 'rcdriscoll@gmail.com', 
+	'Admin', 1-APR-21, NULL, 42.42, 21.21, FALSE);
+
+INSERT INTO ACCOUNT
+VALUES('002', 'willyWonka', 'wonkaBar', 'wonka@gmail.com',
+        'User', 4-MAR-21, 2, 52.76, 54.32, FALSE);
+
+INSERT INTO ACCOUNT
+VALUES('003', 'BilboBagginz', 'jkewlJKEs', 'Bilbo@gmail.com',
+        'User', 11-JUN-20, 1, 89.33, 41.22, FALSE);
+
+INSERT INTO ACCOUNT
+VALUES('004', 'Gandalf', 'Wandz', 'Wizzard4747@gmail.com', 
+        'User', 5-MAR-21, 3, 99.99, 21.21, FALSE);
+
