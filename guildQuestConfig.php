b1af3d64e94ee8c9315067508d8aa6ea0d5a4ff3
rcdriscoll <62107830+rcdriscoll@users.noreply.github.com>
2021-04-13 13:03:59 -0600

    Delete guildQuestConfig.php

--- guildQuestConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "127.0.0.1";			// localhost
-$user = "userName";				// username for MySQL
-$password = "password";			// password for MySQL
-$dbname = "GuildQuest";			// our database name
-$port = 3306				// default port for MySQL
-
-?>
