71b847c5eec440e37e62ebd734def7a1b044ae93
Ryan Driscoll <ryan.driscoll@mines.sdsmt.edu>
2021-04-13 13:03:17 -0600

    refactoring config location

---
+++ www/guildQuestConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "127.0.0.1";			// localhost
+$user = "username";			// username for MySQL
+$password = "pass";			// password for MySQL
+$dbname = "GuildQuest";			// our database name
+$port = 3306				// default port for MySQL
+
+?>
