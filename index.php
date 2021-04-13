ab7e4add1253070cfa7bc74b4ddacb60efde5288
Ryan Driscoll <ryan.driscoll@mines.sdsmt.edu>
2021-04-12 21:24:50 -0600

    implemented sign up form

--- index.php
+++ index.php
@@ -11,14 +11,34 @@
 		<h1>Welcome to GuildQuest!</h1>
 	</div>
         <div class="login">
+	<h2>Login</h2>
 	<form method="POST" action="login.php">
                 <label for="username">Username:</label><br>
                 <input type="text" id="username" name="username"><br>
                 <label for="password">Password:</label><br>
                 <input type="password" id="password" name="password"><br>
                 <input type="submit" id = "loginButton" value = "Login">
-                </form>
-        </div>
+        </form>
+	</div>
+	<br>
+	<br>
+	<br>
+	<div class="signup">
+	<h2>Sign Up</h2>
+	<form method="POST" action="signup.php">
+		<label for="signUpEmail">Email:</label><br>
+		<input type="text" id="signUpEmail" name="signUpEmail"><br>
+		<label for="signUpUser">Username:</label><br>
+		<input type="text" id="signUpUser" name="signUpUser"><br>
+		<label for="signUpPass">Password:</label><br>
+		<input type="password" id="signUpPass" name="signUpPass"><br>
+		<label for="robot">I am not a robot</label>
+		<input type="checkbox"><br>	
+		<input type="submit" id="signupButton" value="Sign Up">
+		
+
+	</form>
+	</div>
 
 </body>
 </html>
