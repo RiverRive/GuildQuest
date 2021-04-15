ff0442cd221b9bfbbbb14dd68fb1aeb125daa514
mushcuffin <you@example.com>
2021-04-14 18:30:43 -0600

    Removed password

--- www/guildQuestConfig.php
+++ www/guildQuestConfig.php
@@ -2,7 +2,7 @@
 
 $host = "127.0.0.1";			// localhost
 $user = "root";				// username for MySQL
-$password = "myParis50sql";		// password for MySQL
+$password = "";		// password for MySQL
 $dbname = "GuildQuest";			// our database name
 $port = 3306				// default port for MySQL
 
